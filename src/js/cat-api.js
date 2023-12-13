b704d8a9ee187801e57a22ed8a448e78340752de
olga-deputat <144669008+olga-deputat@users.noreply.github.com>
2023-12-13 17:03:42 +0200

    finish

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_cNyjISVs1MLXg21zyo3c8W3FVMoEuDt9sffQhFusob9TJiD893KhgDU2b6rmiDyG";
 
-export function fetchBreeds(){
+
+export function fetchBreeds() {
     const BASE_URL = 'https://api.thecatapi.com/v1';
     const END_POINT = '/breeds';
 
     const url = `${BASE_URL}${END_POINT}`;
-    return  axios.get(url);
-};
+    
+    return axios.get(url);
+}
 
 export function fetchCatByBreed(breedId) {
     const BASE_URL = 'https://api.thecatapi.com/v1';
-    const END_POINT = '/image/search';
-    const params = {
-        params: { breed_ids: breedId },
-    };
+    const END_POINT = '/images/search';
+    const config = { params: { breed_ids: breedId } };
 
     const url = `${BASE_URL}${END_POINT}`;
-    return  axios.get(url, params);
-};
+
+    return axios.get(url, config);
+}
